3c2a12dd460ae3b784b13c6e5f5c9f86b19d5e13
AmrYasserSaber <96260095+AmrYasserSaber@users.noreply.github.com>
2021-12-16 23:42:19 +0200

    Delete Task-1.py

--- Task-1.py
+++
@@ -1,6 +0,0 @@
-L=int(input("please input the length of the rectangle."))
-W=int(input("please input the width of the rectangle"))
-Area=L*W
-perimeter=(2*L)+(2*W)
-print("The Area of the rectangle is {} .".format(Area))
-print("The perimeter of the rectangle is {} .".format(perimeter))
