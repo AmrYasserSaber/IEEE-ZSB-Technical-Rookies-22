a7218fa0e763cba6392901022c8168c2c7642497
AmrYasserSaber <amr.yassr.2@gmail.com>
2021-12-23 13:30:02 +0200

    Task-2[problem-3]

--- Task-2/problem-3.py
+++ Task-2/problem-3.py
@@ -1,11 +1,12 @@
 import random
-n=random.randint(100,999)
+#n=random.randint(100,999)
+n=427
 n=str(n)
 n=list(n)
 hits=0
 while hits<3:
     hits=0
-    misses=3
+    misses=0
     u=(input("please input your guess:    "))
     u=list(u)
     for i in range(0,3):
@@ -13,7 +14,7 @@ while hits<3:
             hits+=1
         for c in range (0,3):
             if u[i]==n[c]:
-                misses-=1
-
+                misses+=1
+    misses=misses-hits
     print("{0}hits,{1}misses".format(hits,misses))
 print("winner winner chicken diner")
